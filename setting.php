4f8a9fd942497d16132c551b3b144afae4268c16
Ion <i.cojucovschi@software-dep.net>
2018-01-17 16:16:49 +0200

    update seting for resolve conflict wit host

--- setting.php
+++ setting.php
@@ -1,8 +1,13 @@
 <?php
-define('HOST', 'localhost');
+define('HOST', 'localhost');////http://127.0.0.1:81   ///'localhost'
 define('USER', 'root');
 define('PASS', '');
 define('DB','ustiraspol');
 
+/// if you are logged on gostinger you must activate this code and unable from above.
+///define('HOST', 'mysql.hostinger.ro');
+///define('USER', 'u105415954_ion');
+///define('PASS', '4ion1994');
+///define('DB','u105415954_ion');
 
 ?>
