095b1236e76f350eb86f418829d0f116ae57a0fa
admin_pk\Admin <Cojucovschi@gmail.com>
2018-01-26 17:04:41 +0200

    api fixx show all elements from login

--- Pages/api.php
+++ Pages/api.php
@@ -6,22 +6,20 @@ exit( '{"error":"'.$p1.'"," "text":"'.$p2.'"}');
 }
 
 
-
 if($Module=='users'){
-
 if(!$Param['login']) Error(1,'loginul utilizatorului este null');
 if(!$Param['param']) Error(2,'parametrii de identificcare este null');
-/////forma url    api/login/loginul_var/param/paasdas.asdasd.asd
+/////forma url    /api/users/login/ion/param/id
 $Param['login']==FormChars($Param['login']);
 
 $Array= array('name','surname','id' );
 
 $Exp=explode('.', $Param['param']);////lincul se va transforma in array delimitatorul fiind simbolul '.'
 
-foreach ($Exp as $key) if($Param!='all' and !in_array($key,$Array)) Error(3,'parametrii nu sunt introdusi corect');
+foreach ($Exp as $key) if($Param['param']!='all' and !in_array($key,$Array)) Error(3,'parametrii nu sunt introdusi corect');
 
 
-if($Param=='all')$Select=$Array;
+if($Param['param']=='all')$Select=$Array;
 else $Select=$Exp;
 
 
@@ -33,8 +31,7 @@ $SQL=substr($SQL,0,-1);
 ///exit($SQL);
 
 
-$qr=mysqli_query($CONNECT, "SELECT $SQL FROM `users` WHERE `login`='$Param[login]'");
-
+$qr=mysqli_query($CONNECT, "SELECT $SQL FROM `Users` WHERE login='$Param[login]'");
 
 echo json_encode(mysqli_fetch_assoc($qr),JSON_UNESCAPED_UNICODE);
 
